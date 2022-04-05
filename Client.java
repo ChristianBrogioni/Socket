ed627efa16b4cc5c2d4a1707580215d593a3bfaa
ChristianBrogioni <93117532+ChristianBrogioni@users.noreply.github.com>
2022-04-05 17:12:26 +0200

    Initial commit

---
+++ Client.java
@@ -0,0 +1,45 @@
+package gestioneSocket;
+
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Client {
+	
+	public static void main(String[] args) {
+		
+		BufferedReader tastiera; //buffer per l'input da tastiera
+		DataOutputStream outVersoServer; //stream di output
+		BufferedReader inDalServer; //stream di input
+		
+		try {
+			Socket socket = new Socket("127.0.0.1",2000); //apro socket specificando ip e numero di porta del server
+			tastiera= new BufferedReader(new InputStreamReader(System.in)); //InputStreamReader trasforma uno stream di byte in uno stream di caratteri. BufferedReader ci permette di leggere riga per riga
+			outVersoServer= new DataOutputStream(socket.getOutputStream()); //il client pu� scrivere sull'OutputStream
+			inDalServer= new BufferedReader(new InputStreamReader(socket.getInputStream())); //il client pu� ricevere dati dal server leggendo dall'InputStream
+			String messaggioDelServer= inDalServer.readLine(); //lettura dell'InputStream tramite metodo readLine
+			System.out.println("Messaggio del server: "+ messaggioDelServer);
+			System.out.println("Messaggio per il server: ");
+			String messaggioPerServer= tastiera.readLine(); //inserimento di una stringa da tastiera
+			outVersoServer.writeBytes(messaggioPerServer+"\r\n"); //scrittura sull'OutputStream tramite metodo writeBytes. La concatenazione dei caratteri di controllo di fine riga "\r\n" � necessaria per l'utilizzo del metodo readLine()
+			messaggioDelServer= inDalServer.readLine();
+			System.out.println("Messaggio del Server: "+ messaggioDelServer);
+			System.out.println("Chiusura connessione");
+			socket.close(); //chiusura della connessione
+		} catch (UnknownHostException e) { //gestione dell'eccezione UnkonwnHostException (host sconosciuto) 
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.err.println("errore connessione con il server");
+			e.printStackTrace();
+		}
+
+    }
+
+}
---
+++ Server.java
@@ -0,0 +1,49 @@
+package gestioneSocket;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.BindException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.sql.Timestamp;
+
+public class Server {
+	
+	
+	public static void main(String[] args) {
+		
+		BufferedReader inDalClient;
+		DataOutputStream outVersoClient;
+		
+		try {
+			ServerSocket serverSocket=new ServerSocket(2000); //creazione di un'istanza della classa java.net.ServerSocket, va specificato il numero di porta su cui rimanere in ascolto. Realizza il connection socket
+			System.out.println("server avviato correttamente");
+			Socket socket=serverSocket.accept(); //ritorno del metodo accept, è un metodo bloccante (non succede nulla finchè non arriva la richiesta)
+			System.out.println("comunicazione avvenuta correttamente"); //arriviamo qui solo se il client fa richiesta
+			System.out.println(socket); //stampa le caratteristiche dell'oggetto socket
+			inDalClient= new BufferedReader(new InputStreamReader(socket.getInputStream())); //il server può ricevere dati dal client leggendo dall'InputStream
+			outVersoClient= new DataOutputStream(socket.getOutputStream()); //il server può scrivere sull'OutputStream
+			outVersoClient.writeBytes("Ciao Client"+"\r\n"); //La concatenazione dei caratteri di controllo di fine riga "\r\n" è necessaria per l'utilizzo del metodo readLine()
+			String messaggioDelClient= inDalClient.readLine(); //lettura dell'InputStream tramite metodo readLine
+			System.out.println("Messaggio del client: "+ messaggioDelClient);
+			String timeString = new Timestamp(System.currentTimeMillis()).toString(); //ottengo la data e l'ora corrente trasformandola in stringa tramite metodo toString. La trasformo in stringa in modo da poter utilizzare il metodo writeBytes
+			outVersoClient.writeBytes(timeString+"\r\n"); //scrittura sull'OutputStream tramite metodo writeBytes.
+			System.out.println("Chiusura connessione");
+			socket.close(); //chiusura della connessione
+		}
+		catch (BindException e) { //gestione dell'eccezione BindException (porta in cui si sta mettondo in ascolto il server occupata)
+			// TODO Auto-generated catch block
+			System.err.println("porta occupata");
+			e.printStackTrace();
+		}
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.err.println("errore avvio server sulla porta 2000");
+			e.printStackTrace();
+		}
+
+    }
+
+}
